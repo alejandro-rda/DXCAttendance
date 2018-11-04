04e1842b87e46f5efc40f52fabf9c825132678f0
aruiz35 <alejandror@dxc.com>
2018-11-04 11:52:34 -0500

    Add Update Method

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const express = require('express');
+const ObjectID = require("mongodb").ObjectID;
 let router = express.Router();
 const url = 'mongodb://dxc-asistencia:73VbZeHtYmTovwPNzIAOp6AnrhIyhxG80FHNeyvR7MWwNbJAF8oFgLWgsUFpwSeY5avcMMEeSgyNrqYYdTsrtg%3D%3D@dxc-asistencia.documents.azure.com:10255/?ssl=true&replicaSet=globaldb';
 
@@ -26,14 +27,46 @@ router.post("/asistenciaDXC", function(req, res, next) {
     }
 );
 
+router.put("/updateAsistencia", function(req, res, next) {
+
+        let uID = req.body.id;
+        let resourceId = req.body.resource;
+        let endDate = req.body.enddate;
+        let endLocation = req.body.endlocation;
+
+        MongoClient.connect(url, function (err, client) {
+            assert.equal(null, err);
+            let db = client.db('dxc_asistencia');
+            updateDocument(db, function () {
+            } ,uID, resourceId, endDate, endLocation)
+        });
+
+        res.end("Se actualizo el registro de asistencia correctamente.");
+    }
+);
+
 let insertDocument = function (db, callback, resourceID, startDate, startLocation) {
     db.collection('asistance').insertOne({
         "resource": resourceID,
         "startdate": startDate,
-        "startlocation ": startLocation
+        "startlocation ": startLocation,
+        "enddate": null,
+        "endlocation": null,
     }, function (err, result) {
         assert.equal(err, null);
         console.log("Se inserto el registro de asistencia correctamente.");
+    });
+};
+
+let updateDocument = function (db, callback, uID, resourceID, endDate, endLocation) {
+
+    let newvalues = {$set: {enddate: endDate, endlocation: endLocation}};
+    let query = {_id: ObjectID(uID), resource: resourceID};
+
+    db.collection('asistance').updateOne(
+    query,newvalues, function (err, result) {
+        assert.equal(err, null);
+        console.log("Se actualizo el registro de asistencia correctamente.");
     });
 };
 
