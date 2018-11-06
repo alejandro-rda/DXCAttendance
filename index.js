569214ba711ca4de5d34e88a2735d933ec1001c5
aruiz35 <alejandror@dxc.com>
2018-11-05 23:59:12 -0500

    Upgrade getDocuments

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const http = require('http');
 const app = require('./app.js');
-
 let server = http.createServer(app);
-
 let port = process.env.PORT || 1337;
 server.listen(port);
 
--- routes/routes.js
+++ routes/routes.js
@@ -2,31 +2,40 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const express = require('express');
 const ObjectID = require("mongodb").ObjectID;
+const async = require('async');
+const url = 'mongodb://dxc-asistencia-2:8wDyfljQlOopuzGRrriq0JU3xFh1CNIPfWUTbubYD58S4E7XQZdomFY8MFfg6gQVBe2fEBLKv9hs0HtkzdyCEw%3D%3D@dxc-asistencia-2.documents.azure.com:10255/?ssl=true&replicaSet=globaldb';
 let router = express.Router();
-const url = 'mongodb://dxc-asistencia:73VbZeHtYmTovwPNzIAOp6AnrhIyhxG80FHNeyvR7MWwNbJAF8oFgLWgsUFpwSeY5avcMMEeSgyNrqYYdTsrtg%3D%3D@dxc-asistencia.documents.azure.com:10255/?ssl=true&replicaSet=globaldb';
+let db = null;
+
+MongoClient.connect(url, function (err, client) {
+    if (err) {
+        console.error(err)
+    }
+    db = client.db('dxc_asistencia');
+    console.log("Conexion realizada");
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.end("The service of DXC-Attendance it works!");
 });
 
-router.post("/asistenciaDXC", function(req, res, next) {
+router.post("/asistenciaDXC", function (req, res, next) {
 
         let resourceId = req.body.resource;
         let startDate = req.body.startdate;
         let startLocation = req.body.startlocation;
 
-        MongoClient.connect(url, function (err, client) {
-            assert.equal(null, err);
-            let db = client.db('dxc_asistencia');
-            insertDocument(db, function () {
-            }, resourceId, startDate, startLocation, res);
-        });
+        insertDocument(function () {
+        }, resourceId, startDate, startLocation, res);
     }
 );
 
-let insertDocument = function (db, callback, resourceID, startDate, startLocation, response) {
-    db.collection('asistance').insertOne({
+let insertDocument = function (callback, resourceID, startDate, startLocation, response) {
+
+    let ModelAsistencia = db.collection("asistance");
+
+   ModelAsistencia.insertOne({
         "resource": resourceID,
         "startdate": startDate,
         "startlocation ": startLocation,
@@ -37,35 +46,100 @@ let insertDocument = function (db, callback, resourceID, startDate, startLocatio
         console.log("Se inserto el registro de asistencia correctamente.");
         let lastIDInserted = result.insertedId.toString();
         response.end(lastIDInserted);
-    },);
+    });
 };
 
-router.put("/updateAsistencia", function(req, res, next) {
+router.put("/updateAsistencia", function (req, res, next) {
 
         let uID = req.body.id;
         let endDate = req.body.enddate;
         let endLocation = req.body.endlocation;
 
-        MongoClient.connect(url, function (err, client) {
-            assert.equal(null, err);
-            let db = client.db('dxc_asistencia');
-            updateDocument(db, function () {
-            } ,uID, endDate, endLocation);
-        });
-    res.end("Se actualizo el registro de asistencia correctamente.");
+        updateDocument(function () {
+            res.end("Se actualizo el registro de asistencia correctamente.")
+        }, uID, endDate, endLocation)
+        ;
     }
 );
 
-let updateDocument = function (db, callback, uID, endDate, endLocation) {
+let updateDocument = function (callback, uID, endDate, endLocation) {
+
+    let ModelAsistencia = db.collection("asistance");
 
     let newvalues = {$set: {enddate: endDate, endlocation: endLocation}};
     let query = {_id: ObjectID(uID)};
 
-    db.collection('asistance').updateOne(
-    query,newvalues, function (err, result) {
-        assert.equal(err, null);
-        console.log("Se actualizo el registro de asistencia correctamente.");
-    });
+    ModelAsistencia.updateOne(
+        query, newvalues, function (err, result) {
+            assert.equal(err, null);
+            console.log("Se actualizo el registro de asistencia correctamente.");
+        });
 };
+
+router.get("/asistenciaDiaxRecurso/:resourceID", function (req, res, next) {
+
+        let ModelAsistencia = db.collection("asistance");
+        let var_resource = req.params.resourceID;
+
+        let today = new Date();
+        let dd = today.getDate();
+        let mm = today.getMonth() + 1;
+        let yyyy = today.getFullYear();
+        if (dd < 10) {
+            dd = '0' + dd;
+        }
+        if (mm < 10) {
+            mm = '0' + mm;
+        }
+        today = yyyy + '-' + mm + '-' + dd;
+
+
+        let query = {
+            resource: var_resource,
+            startdate: new RegExp(today)
+        };
+
+        let yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() - 1);
+        dd = yesterday.getDate();
+        mm = yesterday.getMonth() + 1;
+        yyyy = yesterday.getFullYear();
+        if (dd < 10) {
+            dd = '0' + dd;
+        }
+        if (mm < 10) {
+            mm = '0' + mm;
+        }
+
+        yesterday = yyyy + '-' + mm + '-' + dd;
+        let queryYesterday = {
+            resource: var_resource,
+            startdate: new RegExp(yesterday),
+            enddate: null
+        };
+
+        async.parallel({
+            assistanceToday: function (cb) {
+                ModelAsistencia.find(query).toArray(cb)
+            },
+            assistanceLastYesterday: function (cb) {
+                ModelAsistencia.find(
+                    queryYesterday,
+                    {sort: {startdate: -1}, limit: 1}).toArray(cb);
+            }
+        }, function (err, results) {
+            if (results.assistanceToday.length > 0) {
+                res.end(res.json(results.assistanceToday));
+            } else {
+                if (results.assistanceLastYesterday.length > 0) {
+                    res.end(res.json(results.assistanceLastYesterday))
+                }
+                else {
+                    res.end(res.json("No se encuentran registros de asistencia del dia."));
+                }
+            }
+        });
+    }
+);
 
 module.exports = router;
