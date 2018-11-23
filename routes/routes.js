40e61edf80dac7f3a47c73f9be64c75243bcdb8f
aruiz35 <alejandror@dxc.com>
2018-11-23 16:34:57 -0500

    Upgrade Date Methods 4/4

--- routes/routes.js
+++ routes/routes.js
@@ -183,7 +183,7 @@ router.get("/asistenciaDiaxRecurso/:resourceID&:currDate", function (req, res, n
                             startdate: {$regex: currDate, $options: 'g'},
                         }
                     },
-                    {$sort: {stardate: 1, enddate: 1}},
+                    {$sort: {start: 1, end: 1}},
                 ]).toArray(cb)
             },
             assistanceLastYesterday: function (cb) {
@@ -297,7 +297,7 @@ router.get("/asistenciaHistoricoxRecurso/:resourceID&:startDate&:endDate", funct
                             validEndDate: true
                         }
                     },
-                    {$sort: {stardate: 1, enddate: 1}}
+                    {$sort: {start: 1, end: 1}}
                 ]).toArray(cb);
             }
         }, function (err, results) {
